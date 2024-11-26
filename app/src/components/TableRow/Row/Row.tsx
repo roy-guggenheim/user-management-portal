3e565bd0c90af223d1aacd6b0e16d434654c9b34
roy-guggenheim <105651803+roy-guggenheim@users.noreply.github.com>
2024-11-26 16:15:35 +0200

    Row: initial commit.

---
+++ app/src/components/TableRow/Row/Row.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Row.scss";
+
+type RowProps = {
+    avatar: string;
+    fullName: string;
+    age: number;
+    location: string;
+    email: string;
+    onDetailsClick: () => void;
+};
+
+function Row(props: RowProps) {
+    return (
+        <button className="row">
+            <div className="cell-1">
+                <div className="avatar-wrapper">
+                    <img src={props.avatar} alt="User avatar" />
+                </div>
+                {props.fullName}
+            </div>
+            <div className="cell-2">
+                {props.email}
+            </div>
+            <div className="cell-3">
+                {props.age}
+            </div>
+            <div className="cell-4">
+                {props.location}
+            </div>
+        </button>
+    );
+}
+
+export default Row;
