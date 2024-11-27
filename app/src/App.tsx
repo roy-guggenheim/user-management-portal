12f03dee6f71c069aeeec61db8bab9672e27bab0
roy-guggenheim <105651803+roy-guggenheim@users.noreply.github.com>
2024-11-27 13:58:17 +0200

    App: update for test CreatorCard.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,58 @@
-import ToggleView from './components/ToggleView/ToggleView.tsx';
+import React from 'react';
+import CreatorCard from './components/CreatorCard/CreatorCard.tsx';
 import { useState } from 'react';
 import { ViewState } from './components/ToggleView/ToggleViewState.ts';
 import './App.scss';
 
+const avatarDefaultIcon = 'https://avatars.githubusercontent.com/u/105651803?v=4';
 const initialView = 'grid';
 
 function App() {
   const [currentView, setCurrentView] = useState<ViewState>(initialView);
 
   return (
-    <ToggleView currentView={currentView} onToggle={setCurrentView} />
+    <div className="grid">
+      <CreatorCard
+        backgroundImageUrl={'https://picsum.photos/350/250'}
+        avatarUrl={avatarDefaultIcon}
+        title={'Roy Guggenheim'}
+        subtitlte='28 | Jerusalem'
+        email={'roy.guggen@gmail.com'}
+        onDetailsClick={() => console.log('Button clicked!')}
+      ></CreatorCard>
+      <CreatorCard
+        backgroundImageUrl={'https://picsum.photos/350/250'}
+        avatarUrl={avatarDefaultIcon}
+        title={'Roy Guggenheim'}
+        subtitlte='28 | Jerusalem'
+        email={'roy.guggen@gmail.com'}
+        onDetailsClick={() => console.log('Button clicked!')}
+      ></CreatorCard>
+      <CreatorCard
+        backgroundImageUrl={'https://picsum.photos/350/250'}
+        avatarUrl={avatarDefaultIcon}
+        title={'Roy Guggenheim'}
+        subtitlte='28 | Jerusalem'
+        email={'roy.guggen@gmail.com'}
+        onDetailsClick={() => console.log('Button clicked!')}
+      ></CreatorCard>
+      <CreatorCard
+        backgroundImageUrl={'https://picsum.photos/350/250'}
+        avatarUrl={avatarDefaultIcon}
+        title={'Roy Guggenheim'}
+        subtitlte='28 | Jerusalem'
+        email={'roy.guggen@gmail.com'}
+        onDetailsClick={() => console.log('Button clicked!')}
+      ></CreatorCard>
+      <CreatorCard
+        backgroundImageUrl={'https://picsum.photos/350/250'}
+        avatarUrl={avatarDefaultIcon}
+        title={'Roy Guggenheim'}
+        subtitlte='28 | Jerusalem'
+        email={'roy.guggen@gmail.com'}
+        onDetailsClick={() => console.log('Button clicked!')}
+      ></CreatorCard>
+    </div>
   )
 }
 
